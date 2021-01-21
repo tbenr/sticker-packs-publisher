f761837a875f08f78d7b5d74a1801385a7999def
tbenr <enrico.delfante@gmail.com>
2021-01-21 20:14:46 +0100

    i18n on review dialog

--- src/components/NewStickerPackConfirmationDialog/index.tsx
+++ src/components/NewStickerPackConfirmationDialog/index.tsx
@@ -101,36 +101,36 @@ export default function CustomizedDialogs(props: SimpleDialogProps) {
                     <Box flexGrow={1} borderRight={1} color={theme.palette.divider} paddingRight={2}>
                         <Grid container direction="row" justify="space-between" alignItems="stretch" spacing={2}>
                             <Grid item xs={4}>
-                                <Typography variant="subtitle2" color="textSecondary">Author</Typography>
+                                <Typography variant="subtitle2" color="textSecondary">{t('review.artist-name')}</Typography>
                                 <Typography variant="subtitle2" color="textPrimary">{author}</Typography>
                             </Grid>
                             <Grid item xs={4}>
-                                <Typography variant="subtitle2" color="textSecondary">Sticker Pack name</Typography>
+                                <Typography variant="subtitle2" color="textSecondary">{t('review.sticker-pack-name')}</Typography>
                                 <Typography variant="subtitle2" color="textPrimary">{name}</Typography>
                             </Grid>
                             <Grid item xs={4}>
-                                <Typography variant="subtitle2" color="textSecondary">Category</Typography>
+                                <Typography variant="subtitle2" color="textSecondary">{t('review.category')}</Typography>
                                 <Typography variant="subtitle2" color="textPrimary">{categories.map(v=>AvailableCategories[v]).join(', ')}</Typography>
                             </Grid>
                             <Grid item xs={4}>
-                                <Typography variant="subtitle2" color="textSecondary">Price Pack</Typography>
+                                <Typography variant="subtitle2" color="textSecondary">{t('review.price')}</Typography>
                                 <Typography variant="subtitle2" color="textPrimary">{price}</Typography>
                             </Grid>
                             <Grid item xs={4}>
-                                <Typography variant="subtitle2" color="textSecondary">Contribution to Status</Typography>
+                                <Typography variant="subtitle2" color="textSecondary">{t('review.contribution')}</Typography>
                                 <Typography variant="subtitle2" color="textPrimary">{`${contribution/100}%`}</Typography>
                             </Grid>
                             <Grid item xs={4}>
-                                <Typography variant="subtitle2" color="textSecondary">Limited Edition</Typography>
-                                <Typography variant="subtitle2" color="textPrimary">{installations === -1 ? 'Unlimited' : installations}</Typography>
+                                <Typography variant="subtitle2" color="textSecondary">{t('review.limited-edition')}</Typography>
+                                <Typography variant="subtitle2" color="textPrimary">{installations === -1 ? t('review.unlimited') : installations}</Typography>
                             </Grid>
                             <Grid item xs={12}>
-                                <Typography variant="subtitle2" color="textSecondary">Ethereum address or ENS domain to receive SNT</Typography>
+                                <Typography variant="subtitle2" color="textSecondary">{t('review.address-receiving')}</Typography>
                                 <Typography variant="subtitle2" color="textPrimary">{address}</Typography>
                             </Grid>
                         </Grid>
                         <Divider style={{marginTop: theme.spacing(2), marginBottom: theme.spacing(2)}}/>
-                        <Typography variant='subtitle2' color="textSecondary" paragraph>Stickers</Typography>
+                        <Typography variant='subtitle2' color="textSecondary" paragraph>{t('review.stickers')}</Typography>
                         <Box display="flex" flexWrap="wrap" alignContent="center" alignItems="center" flexDirection="row">
                             {stickers.map(hash => 
                                 <Card style={{backgroundColor: theme.palette.primary.light, borderRadius: 8, padding: 4, margin: 4}} elevation={0}><Image ipfs={hash} style={{position: "relative", textAlign: "center", width: 64, height: 64, borderRadius: 6}}/></Card>
@@ -138,7 +138,7 @@ export default function CustomizedDialogs(props: SimpleDialogProps) {
                         </Box>
                     </Box>
                     <Box display="flex" flexDirection="column" alignContent="center" alignItems="center" justifyContent="center" paddingLeft={2}>
-                        <Typography variant='subtitle2' color="textSecondary" paragraph>This is how it will look like in Status</Typography>
+                        <Typography variant='subtitle2' color="textSecondary" paragraph>{t('review.text1')}</Typography>
                         <StickerPackCard preview={{price:price,author:author,name:name,thumbnail:thumbnail,banner:banner}} edit={false}/>
                     </Box>
                 </Box>
@@ -146,7 +146,7 @@ export default function CustomizedDialogs(props: SimpleDialogProps) {
           </DialogContent>
           <DialogActions >
             <Box flexGrow={1} marginX={4}>
-            <Typography align="center" variant='subtitle2' color="textSecondary">Please review the sitcker pack carefully before publishing because most of the data cannot be edited later.</Typography>
+            <Typography align="center" variant='subtitle2' color="textSecondary">{t('review.text2')}</Typography>
             </Box>
             <Button variant="contained" color="primary" onClick={onConfirm}>
               {(t('review.publish'))}
